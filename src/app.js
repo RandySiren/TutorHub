ba9be67b692bed4b4f129016cc7578d1dd926a6d
randysiren <sranmandeep1997@gmail.com>
2020-06-07 05:03:22 -0400

    Added ESLint settings (optional)

--- src/app.js
+++ src/app.js
@@ -46,9 +46,9 @@ app.get('/', (req, res) => {
 /* Setup error handler */
 if (process.env.NODE_ENV === 'development') {
 	// only use in development
-	app.use(errorHandler());
+    app.use(errorHandler());
 } else {
-	app.use((err, req, res, next) => {
+    app.use((err, req, res, next) => {
 		console.error(err);
 		res.status(500).send('Server Error');
 	});
