814aa1d11ead3979a633e08454a26f8ef4732199
randysiren <sranmandeep1997@gmail.com>
2020-06-07 02:31:39 -0400

    testing commit

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ if (process.env.NODE_ENV === 'development') {
 /* Start Express server */
 app.listen(app.get('port'), () => {
 	console.log('%s App is running at http://localhost:%d in %s mode', chalk.green('✓'), app.get('port'), app.get('env'));
-	console.log('  Press CTRL-C to stop\n');
+	console.log('Press CTRL-C to stop.\n');
 });
 
 module.exports = app;
