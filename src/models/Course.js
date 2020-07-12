3993778bb7b5a56cc7162686e42845d4adb07a4d
RandySiren <sranmandeep1997@gmail.com>
2020-07-12 19:08:03 -0400

    Creating course

---
+++ src/models/Course.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const courseSchema = new Schema({
+    courseID: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    school: {
+        type: String,
+        trim: true,
+        default: 'Generic',
+    },
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    level: {
+        type: Number,
+        min: 1,
+        max: 6,
+    },
+    subject: {
+        type: String,
+    },
+    tutors: [
+        {
+            id: String,
+        },
+    ],
+});
+
+courseSchema.index({ courseID: 1, school: 1 }, { unique: true });
+
+/**
+ * TODO: Add search for all tutors/types/etc.
+ */
+
+const Course = mongoose.model('Course', courseSchema, 'course');
+
+module.exports = { Course };
