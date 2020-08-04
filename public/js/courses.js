a24d01f7f1c9c72e7d07fdc17c134e9a5e97b409
Brandon Daliri <brandonkdaliri@gmail.com>
2020-08-04 18:49:56 -0400

    updated add-course card info to match my-courses

--- public/js/courses.js
+++ public/js/courses.js
@@ -150,7 +150,7 @@ async function createCourseAdd(parentDiv) {
                         <div class="col-md-4 col-sm-4">
                             <div class="panel panel-default">
                                 <div class="panel-heading">
-                                    ${courseData.courseId}<br /> <i>${courseData.name}</i>
+                                    ${courseData.courseId}: ${courseData.name}<br /> <i>${courseData.school}</i>
                                 </div>
                                 <div class="panel-footer">
                                     <button class="btn btn-warning" data-toggle="modal" data-target="#myModal${index}">
